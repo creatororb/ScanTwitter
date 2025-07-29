e568fed56341ff2003ccbf7ee14f4af17296e537
creatororb <adrenaline.low@gmail.com>
2025-07-29 19:52:53 +0000

    Add main.py with FastAPI Twitter scanner

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+import snscrape.modules.twitter as sntwitter
+from typing import List
+
+app = FastAPI()
+
+class Tweet(BaseModel):
+    date: str
+    username: str
+    content: str
+
+@app.get("/search_tweets", response_model=List[Tweet])
+async def search_tweets(
+    query: str = Query(..., description="Search query (keyword or hashtag)"),
+    max_tweets: int = Query(100, description="Max number of tweets to fetch")
+):
+    tweets = []
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+        if i >= max_tweets:
+            break
+        tweets.append(Tweet(
+            date=str(tweet.date),
+            username=tweet.user.username,
+            content=tweet.content
+        ))
+    return tweets
